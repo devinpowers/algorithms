588660d4347a7ffc7d3e1e45bdc0bf2a94d3ae77
Devin Powers <powers88@msu.edu>
2020-08-26 13:49:44 -0400

    add projects

---
+++ Chapter3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 26 13:13:52 2020
+
+@author: devinpowers
+"""
+
+from time import time
+
+start = time()
+x = [i for i in range(10)]
+print(x)
+
+end = time()
+
+
+elapsed = end - start
