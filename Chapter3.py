5958f9559566da4e066290ea54ef33ede8820040
Devin Powers <powers88@msu.edu>
2020-08-27 09:07:23 -0400

    chapter 5

--- Chapter3.py
+++ Chapter3.py
@@ -27,3 +27,97 @@ for k in range(10):
     data.append(None)
     
     
+import ctypes # provides low-level arrays 
+
+class DynamicArray:
+    
+    def __init__(self):
+        self._n = 0
+        self._capacity = 1
+        self._A = self._make_array(self._capacity)
+    
+    def __len__(self):
+        
+        return self._n
+    
+    def __getitem__(self,k):
+        
+        if not 0 <=k < self._n:
+            raise IndexError('Invalid index')
+        
+        return self._A[k]
+    
+    def append(self,obj):
+        
+        if self._n == self._capacity:
+            self._resize(2*self._capacity)
+            self._A[self._n] = obj
+            self._n += 1
+    
+    def  _resize(self, c):
+        
+        '''”Resize internal array to capacity c.””'''
+        B = self._make_array(c)
+        
+        for k in range(self._n):
+            B[k] = self._A[k]
+        self._A = B
+        self._capacity = c
+    
+    def _make_array(self,c):
+        
+        return(c*ctypes.py_object)()
+        
+        
+devin = DynamicArray()
+
+devin._make_array(12)
+
+
+# Python list class
+
+from time import time
+def compute_average(n):
+    data =[]
+    start = time()
+    for k in range(n):
+        data.append(None)
+    
+    end = time()
+    return (end-start)/ n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
