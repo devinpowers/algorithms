6922b025994fd58d6726e6b0633a1babb7c8c8df
Devin Powers <powers88@msu.edu>
2020-09-03 17:14:05 -0400

    merge sort 2 python file

---
+++ Sorting Algorithms/Merge_Sort_2.py
@@ -0,0 +1,77 @@
+
+
+
+def Merge (Left, Right, Array):
+    
+    Left_length = len(Left)
+    
+    Right_length = len(Right)
+    
+    i = j = k = 0
+    
+    while (i < Left_length and j < Right_length):
+        
+        if Left_length[i] <= Right_length[j]:
+            
+            Array[k] = Left_length[i]
+            i = i + 1
+        
+        else:
+            Array[k] = Right_length[j]
+            j = j+ 1
+            
+        
+        k = k + 1
+    
+    # in case one of the letters runs out before the other
+    while i < Left_length:
+        
+        Array[k] = Left_length[i]
+        i = i + 1
+        k = k + 1
+        
+    while j < Right_length:
+        
+        Array[k] = Right_length[j]
+        j = j + 1
+        k = k + 1
+        
+    return Array
+
+
+
+def Merge_Sort(Array):
+    
+    n = len(Array)
+    
+    if n < 2:
+
+        return
+    
+    mid = n // 2
+    
+    Left = Array[:mid]
+    
+    Right = Array[mid::]
+    
+    
+    for i in range(0:mid):
+        
+        Left[i] = A[i]
+        
+    for i in range(mid: n-1):
+        
+        Right[i-mid] = A[i]
+        
+    Merge_Sort(Left)
+    
+    Merge_Sort(Right)
+
+
+    Merge(Left, Right, Array)    
+    
+    
+n = [2,4,1,6,8,5,3,7]
+
+print(Merge_Sort(n))
+    
