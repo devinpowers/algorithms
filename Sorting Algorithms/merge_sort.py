ce3ecee8df68a3dc775171f2c22f36fca92e00a1
Devin Powers <powers88@msu.edu>
2020-09-04 14:05:47 -0400

    finished my merge sort algorithm

--- Sorting Algorithms/merge_sort.py
+++ Sorting Algorithms/merge_sort.py
@@ -1,112 +1,68 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  2 14:29:10 2020
 
-@author: devinpowers
-"""
+'''Merge Sort Function Built By ME!!!!!'''
 
-
-
-def merge(S1, S2, S):
-  """Merge two sorted Python lists S1 and S2 into properly sized list S."""
-  i = j = 0
-  while i + j < len(S):
-    if j == len(S2) or (i < len(S1) and S1[i] < S2[j]):
-      S[i+j] = S1[i]      # copy ith element of S1 as next item of S
-      i += 1
-    else:
-      S[i+j] = S2[j]      # copy jth element of S2 as next item of S
-      j += 1
-
-def merge_sort(S):
-  """Sort the elements of Python list S using the merge-sort algorithm."""
-  n = len(S)
-  if n < 2:
-    return                # list is already sorted
-  # divide
-  mid = n // 2
-  S1 = S[0:mid]           # copy of first half
-  S2 = S[mid:n]           # copy of second half
-  # conquer (with recursion)
-  merge_sort(S1)          # sort copy of first half
-  merge_sort(S2)          # sort copy of second half
-  # merge results
-  merge(S1, S2, S)        # merge sorted halves back into S
-
-
-S = [85,24,63,45,17,31,96,50]
-
-merge_sort(S)
-
-print(S)
-
-'''Returned'''
-
-
-def msort(x):
-    result = []
-    if len(x) < 2:
-        return x
-    mid = int(len(x)/2)
-    y = msort(x[:mid])
-    z = msort(x[mid:])
-    while (len(y) > 0) or (len(z) > 0):
-        if len(y) > 0 and len(z) > 0:
-            if y[0] > z[0]:
-                result.append(z[0])
-                z.pop(0)
-            else:
-                result.append(y[0])
-                y.pop(0)
-        elif len(z) > 0:
-            for i in z:
-                result.append(i)
-                z.pop(0)
+def Merge(Left, Right, Array):
+    
+    Left_length = len(Left)
+        
+    Right_length = len(Right)
+    
+    i = j = k = 0
+        
+    while (i < Left_length and j < Right_length):
+        
+        if Left[i] <= Right[j]:
+            Array[k] = Left[i]
+            i = i + 1
+        
         else:
-            for i in y:
-                result.append(i)
-                y.pop(0)
-    return result
+            Array[k] = Right[j]  
+            j = j+ 1
+        k = k + 1
+        
+    # in case one of the letters runs out before the other
+    while i < Left_length:
+        
+        Array[k] = Left[i]
+        i = i + 1
+        k = k + 1
+        
+    while j < Right_length:
+        
+        Array[k] = Right[j]
+        j = j + 1
+        k = k + 1
+        
+    return Array
 
+def Merge_Sort(Array): 
+    '''Recursive function to sort an Array'''
+    n = len(Array)
+    
+    if n < 2:    
+        return
+    middle = n // 2
+    
+        # have to make right and left subarrays        
+    Left = Array[0:middle]
+          
+    Right = Array[middle:]
+  
+        #Recursive functions
 
+    Merge_Sort(Left)
+    
+    Merge_Sort(Right)
+      
+    # calling the merge function
+    Merge(Left, Right, Array)
+    
+a = [2,4,1,6,8,5,3,7]
 
-f = [2,31,31,4,231,231,4,20,23,0]
-print(msort(f))
+Merge_Sort(a)
 
-'''https://www.edureka.co/blog/python-program-merge-sort/#:~:text=Advantages%20and%20Usage-,What%20is%20Merge%20Sort%20in%20Python%3F,for%20merging%20the%20sorted%20arrays.'''
-def mergeSort(nlist):
-    print("Splitting ",nlist)
-    if len(nlist)>1:
-        mid = len(nlist)//2
-        lefthalf = nlist[:mid]
-        righthalf = nlist[mid:]
- 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-        i=j=k=0      
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                nlist[k]=lefthalf[i]
-                i=i+1
-            else:
-                nlist[k]=righthalf[j]
-                j=j+1
-            k=k+1
- 
-        while i < len(lefthalf):
-            nlist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
- 
-        while j < len(righthalf):
-            nlist[k]=righthalf[j]
-            j=j+1
-            k=k+1
-    print("Merging ",nlist)
- 
-nlist = [3,1,4,1,5,9,2,6,5,4]
-mergeSort(nlist)
-print(nlist)
+print(a)
+
+'Results: [1, 2, 3, 4, 5, 6, 7, 8]'
 
 
