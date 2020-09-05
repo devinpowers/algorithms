56088a5e1f99e5469cef3bc68985559514201836
Devin Powers <powers88@msu.edu>
2020-09-05 18:41:48 -0400

    new quick sort notes

--- Sorting Algorithms/QuickSort.py
+++ Sorting Algorithms/QuickSort.py
@@ -1,30 +1,52 @@
 
+def partition(arr, low, high):
+    i = (low-1)         # index of smaller element
+    pivot = arr[high]     # pivot
+ 
+    for j in range(low, high):
+ 
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+ 
+            # increment index of smaller element
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+ 
+
+
 
 ''' QuickSort '''
 
 
 
 def Partition(A, Start, End):
+    
     '''Selecting the Pivot and Stuff'''
+    
+    i = (Start -1)
     
     Pivot = A[End]
     
-    p_index = A[Start-1]
+
     
-    for i in range(Start, End):
-        if A[i] <= Pivot:
-            A[i] = A[p_index]
+    for j in range(Start, End):
         
-        p_index = p_index +1
-    
-    A[p_index] = A[End]
-    
-    return p_index
-
-    
+       
+        if A[j] <= Pivot:
+            i = i +1
+            
+            A[i], A[j] = A[j], A[i]
     
     
-
+    A[i+1], A[End] = A[End], A[i+1]
+    
+    p_index = i 
+    
+    return (p_index +1)
 
 
 def QuickSort(Array, Start, End):
@@ -34,17 +56,20 @@ def QuickSort(Array, Start, End):
     
     p_index = Partition(Array,Start,End)
     
-    
     QuickSort(Array, Start, p_index -1)
     
     QuickSort(Array, p_index+1, End)
+    
+    
+    
 
 
 a = [3,4,2,31,223,12,0,40]
 
 n = len(a)
 
-QuickSort(a,0, n-1)
+print(QuickSort(a,0, n-1))
+
 
 
     
