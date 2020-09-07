4b836ac941ec508839db70ff41bddd8341301223
Devin Powers <powers88@msu.edu>
2020-09-07 14:42:33 -0400

    working on project one, using merge sort

---
+++ Projects/Project 1/Project_One.py
@@ -0,0 +1,26 @@
+
+
+'''Project One'''
+
+
+
+class Person(self, other):
+    pass
+
+
+def order_first_name():
+    pass
+
+def order_last_name():
+    pass
+
+def is_alphabetized():
+    
+    '''Should rn in Big Oh (n) time and'''
+    pass
+
+def alphabetize():
+    '''will return the number of comparisons made '''
+    pass
+
+
