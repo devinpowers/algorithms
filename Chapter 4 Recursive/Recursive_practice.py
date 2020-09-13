87fcef6529e80d353ff1f14f56b566d8e6074da3
Devin Powers <powers88@msu.edu>
2020-09-13 15:41:00 -0400

    new recursive code notes

--- Chapter 4 Recursive/Recursive_practice.py
+++ Chapter 4 Recursive/Recursive_practice.py
@@ -1,13 +1,8 @@
-
-
-
-
-
-
-
+'''Recursive Practice'''
 
 
 def fact(n):
+    
     # assuming that n is a positive integer or 0
     if n >= 1:
         return n * fact(n - 1)
@@ -16,23 +11,17 @@ def fact(n):
 
 
 
-print("0! =", fact(0))
-#print("1! =", fact(1))
-#print("2! =", fact(2))
-#print("3! =", fact(3))
-fact(4)
-#print("4! =", fact(4))
+devin = fact(3)
 
-
-
-
-
-
-
-
-
+print(devin)
 
 '''
+print("0! =", fact(0))
+print("1! =", fact(1))
+print("2! =", fact(2))
+print("3! =", fact(3))
+
+
 
 def fib(n):
     # assuming that n is a positive integer
@@ -47,6 +36,6 @@ print("fib(3) =", fib(3))
 print("fib(4) =", fib(4))
 print("fib(5) =", fib(5))
 
-'''
 
+'''
 
