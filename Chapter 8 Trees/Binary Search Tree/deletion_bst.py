e9c24dc99f5ed201a68b4432dff3c7466a3a6ab4
Devin Powers <powers88@msu.edu>
2020-10-28 15:21:02 -0400

    bst deletion

---
+++ Chapter 8 Trees/Binary Search Tree/deletion_bst.py
@@ -0,0 +1,157 @@
+class Node(object):
+    
+   def __init__(self, data = None):
+        
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    
+    def __init__(self):
+        
+        self.root = None
+
+    def insert(self, data):
+        
+        if self.root is None:
+            self.root = Node(data)           
+        else:
+            self._insert(data,self.root)
+
+    def _insert(self, data, current_node):
+        
+        if data < current_node.data:       
+            if current_node.left is None:           
+                current_node.left = Node(data)           
+            else:
+                self._insert(data, current_node.left)   
+        elif data > current_node.data:       
+            if current_node.right is None:           
+                current_node.right = Node(data)      
+            else:
+                self._insert(data, current_node.right)           
+        else:
+            print("Value is Already present in Tree.")
+            
+            
+    def print_tree(self, traversal_type):
+        
+
+        if traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False    
+            
+    def inorder_print(self, start, traversal):
+
+        if start:
+
+            traversal = self.inorder_print(start.left, traversal)
+
+            traversal += (str(start.data) + "-")
+
+            traversal = self.inorder_print(start.right, traversal)
+
+        return traversal
+    
+
+
+            
+    def minValueNode(self,node): 
+        
+        '''Find the minimum value'''
+        
+        current = node 
+  
+        while current.left is not None: 
+            
+            current = current.left  
+            
+        return current  
+        
+
+    def delete_node(self, current_node, data):
+        
+        # Base Case
+     
+        if current_node is None:
+            
+            return current_node
+        
+        # if data to be deleted is smaller the root is lies in the left subtree
+        
+        if data < current_node.data:
+            
+            current_node.left = self.delete_node(current_node.left, data)
+        
+        # elif most lie in the right subtree
+        elif data > current_node.data:
+            
+            current_node.right = self.delete_node(current_node.right, data)
+        
+        # if the value is the same as root data 
+        
+        else:
+            print('Root Value is:', current_node.data)
+            
+            
+            # Node with only one child or no child
+            
+            if current_node.left is None:
+                
+                temp = current_node.right
+                
+                current_node = None
+                
+                return temp
+            
+            elif current_node.right is None:
+                temp = current_node.left
+                current_node =None
+                return temp
+            
+            
+            
+            temp = self.minValueNode(current_node.right) 
+  
+            # Copy the inorder successor's content to this node 
+            current_node.data = temp.data 
+  
+            # Delete the inorder successor 
+            current_node.right = self.delete_node(current_node.right , temp.data) 
+            
+        return current_node 
+           
+           
+tree = BST()
+
+tree.insert(50)
+tree.insert(30)
+tree.insert(20)
+tree.insert(40)
+tree.insert(70)
+tree.insert(60)
+tree.insert(80)
+
+# delete 20
+tree.delete_node(tree.root,20)
+
+print(tree.print_tree("inorder"))
+
+#delete 30
+
+tree.delete_node(tree.root, 30)
+print(tree.print_tree("inorder"))
+
+#deletee 50
+tree.delete_node(tree.root, 50)
+
+print(tree.print_tree("inorder"))
+
+
+            
+            
+            
--- Chapter 8 Trees/Binary Search Tree/search_insert_bst.py
+++ Chapter 8 Trees/Binary Search Tree/search_insert_bst.py
@@ -2,47 +2,29 @@
  
 # A utility class that represents an individual node in a BST 
 class Node: 
-    def __init__(self,key): 
+    
+    def __init__(self,value): 
+        
         self.left = None
         self.right = None
-        self.val = key 
- 
-# A utility function to insert a new node with the given key 
-def insert(root,key): 
-    if root is None: 
-        return Node(key) 
-    else:
-        if root.val == key:
-            return root
-        elif root.val < key: 
-            root.right = insert(root.right, key) 
+        self.val = value
+        
+class BST:
+    
+    def __init__(self):
+        
+        self.root = None
+
+    def insert(self, value):
+        
+        if self.root is None:
+            
+            self.root = Node(data)
+            
         else:
-            root.left = insert(root.left, key)
-    return root
- 
-# A utility function to do inorder tree traversal 
-def inorder(root): 
-    if root: 
-        inorder(root.left) 
-        print(root.val) 
-        inorder(root.right) 
- 
- 
-# Driver program to test the above functions 
-# Let us create the following BST 
-#    50 
-#  /     \ 
-# 30     70 
-#  / \ / \ 
-# 20 40 60 80 
- 
-r = Node(50) 
-r = insert(r, 30) 
-r = insert(r, 20) 
-r = insert(r, 40) 
-r = insert(r, 70) 
-r = insert(r, 60) 
-r = insert(r, 80) 
- 
-# Print inoder traversal of the BST 
-inorder(r) 
+            if root.val == value
+            
+                return root
+            
+            elif root.val <
+            
