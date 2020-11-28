b26d9edbfe0c934e7516b40562328ccd8ae9f73a
Devin Powers <powers88@msu.edu>
2020-11-28 18:14:26 -0500

    heap

--- Priority Queues/Heap Project/heap_project.py
+++ Priority Queues/Heap Project/heap_project.py
@@ -2,6 +2,31 @@
 
 
 class Heap:
-    """Heap Based Queue"""
-    
-    
+
+    """
+    A heap-based priority queue
+    Items in the queue are ordered according to a comparison function
+    """
+
+    def __init__(self, comp):
+
+        """
+        Constructor
+        :param comp: A comparison function determining the priority of the
+        included elements
+        """
+
+        self.comp = comp
+        # Added Members
+
+        self.h = []
+        self.size = 0
+
+    def __len__(self):
+
+        """
+        Length of heap
+        :return: length of heap
+        """
+
+        return self.size 
