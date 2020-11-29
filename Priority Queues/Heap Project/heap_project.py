1e8f7e4ccab325915f0b0ce948065147c3ecc457
Devin Powers <powers88@msu.edu>
2020-11-29 13:03:04 -0500

    bubble

--- Priority Queues/Heap Project/heap_project.py
+++ Priority Queues/Heap Project/heap_project.py
@@ -60,8 +60,7 @@ class Heap:
         return 'Heap([{0}])'.format(','.join(str(item) for item in self))
 
     
-    
-    
+
     def peek(self):
         
         "Returns the Min value/ Root Value"
@@ -79,4 +78,59 @@ class Heap:
         
         self.h.append(item)
         
-        i = len(self)
+        
+        i = len(self) - 1
+        
+        while i > 0 and self.comp(item, self.h[parent(i)]):
+            
+            self.h[i] = self.h[parent(i)]
+
+            i = parent(i)
+            
+        self.h[i] = item
+        
+    
+    
+    
+    def clear(self):
+        
+        """Resets the heap to be empty"""
+        
+        self.h = []
+        self.size = 0
+        
+        
+    def heapify(self, key):
+
+        """
+        Heapifies current heap starting at key in place
+        :param key: index to start at
+        :return: None
+        """
+
+        left = 2 * key + 1
+        right = 2 * key + 2
+
+        if left < len(self) and self.comp(self.h[left], self.h[key]):
+
+            small = left
+
+        else:
+
+            small = key
+
+        if right < len(self) and self.comp(self.h[right], self.h[small]):
+
+            small = right
+
+        if small != key:
+
+            self.h[key], self.h[small] = self.h[small], self.h[key]
+
+            self.heapify(small)
+      
+        
+        
+        
+        
+    
