4f5e3d9667f99d2fbab04d878518aaf6c51d1eca
Devin Powers <powers88@msu.edu>
2020-12-01 13:56:32 -0500

    fix;

--- Priority Queues/Heap Project/heap_project.py
+++ Priority Queues/Heap Project/heap_project.py
@@ -100,38 +100,166 @@ class Heap:
         self.size = 0
         
         
-    def heapify(self, key):
 
+    def __iter__(self):
         """
-        Heapifies current heap starting at key in place
-        :param key: index to start at
-        :return: None
+        An iterator for this heap
+        :return: An iterator
         """
+        return iter(self.data)
 
-        left = 2 * key + 1
-        right = 2 * key + 2
+    # Supplied methods
 
-        if left < len(self) and self.comp(self.h[left], self.h[key]):
+    def __bool__(self):
+        """
+        Checks if this heap contains items
+        :return: True if the heap is non-empty
+        """
+        return not self.is_empty()
 
-            small = left
+    def is_empty(self):
+        """
+        Checks if this heap is empty
+        :return: True if the heap is empty
+        """
+        return len(self) == 0
 
-        else:
+    def __repr__(self):
+        """
+        A string representation of this heap
+        :return:
+        """
+        return 'Heap([{0}])'.format(','.join(str(item) for item in self))
 
-            small = key
 
-        if right < len(self) and self.comp(self.h[right], self.h[small]):
 
-            small = right
 
-        if small != key:
 
-            self.h[key], self.h[small] = self.h[small], self.h[key]
+    # Added methods
+    def sift_up(self, i):
+        """
+        Move elements in the heap until they meet criteria
+        :param i: index of element
+        """
+        p = parent(i)
+        # Only swap if not at first element and need to swap
+        if i > 0 and self.comp(self.data[i], self.data[p]):
+            # Swap
+            self.swap(i, p)
+            self.sift_up(p)
 
-            self.heapify(small)
-      
-        
+    def swap(self, i, j):
+        """
+        Swap two elements in heap
+        :param i: index of first elem
+        :param j: index of second elem
+        """
+        self.data[i], self.data[j] = self.data[j], self.data[i]
+
+    def sift_down(self, i):
+        """
+        Move elements in heap until they meet criteria
+        :param i: index
+        """
+        if self.has_left(i):
+            l = left(i)
+            smallest_child = l
+            # Check if right child is smaller
+            if self.has_right(i):
+                r = right(i)
+                if self.comp(self.data[r], self.data[l]):
+                    smallest_child = r
+            # Check if swap smallest child
+            if self.comp(self.data[smallest_child], self.data[i]):
+                self.swap(i, smallest_child)
+                self.sift_down(smallest_child)
+
+    def has_left(self, i):
+        """
+        check if item has left child
+        :param i: item to check
+        :return: True or False
+        """
+        return left(i) < len(self.data)
+
+    def has_right(self, i):
+        """
+        check if item has right child
+        :param i: item to check
+        :return: True or False
+        """
+        return right(i) < len(self.data)
+
+
+
             
-        kava
+        
         
 
-    
+
+def find_median(seq):
+    """
+    Finds the median (middle) item of the given sequence.
+    Ties are broken arbitrarily.
+    Strategy - extend seq to each heap then do n//2+1 extractions to determine the median
+    :param seq: an iterable sequence
+    :return: the median element
+    """
+    # If seq is empty
+    if not seq:
+        raise IndexError
+
+    # Extend seq to each heap. Affected by lambda
+    min_heap = Heap(lambda a, b: a < b)
+    min_heap.extend(seq)
+    max_heap = Heap(lambda a, b: a > b)
+    max_heap.extend(seq)
+
+    # Doing n//2+1 returns the median
+    for _ in range(len(max_heap)//2+1):
+        max_mid = max_heap.extract()
+    for _ in range(len(min_heap)//2+1):
+        min_mid = min_heap.extract()
+
+    # If seq is even, can return either median item
+    if len(seq)%2 == 1:
+        return max_mid
+    return min_mid
+
+
+def parent(i):
+    """
+    Return the parent index of given index
+    :param i: item to get parent of
+    :return: index of parent
+    """
+    return (i - 1) // 2
+
+
+def left(i):
+    """
+    Return the left child index of given index
+    :param i: item to get left child of
+    :return: index of left child
+    """
+    return 2 * i + 1
+
+
+def right(i):
+    """
+    Return the right child index of given index
+    :param i: item to get right child of
+    :return: index of right child
+    """
+    return 2 * i + 2
+
+
+def main(filename):
+    with open(filename, 'r') as reader:
+        text = [line.strip() for line in reader]
+        median = find_median(text)
+        print('Median:', median)
+
+
+if __name__ == '__main__':
+    main('alphabet.txt')
