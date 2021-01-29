7ed651ab22937f04d48e1aadafe31743adbd133f
Devin Powers <powers88@msu.edu>
2021-01-29 08:39:37 -0500

    algorithms

---
+++ extras/iterators and generators/people.py
@@ -0,0 +1,48 @@
+
+
+import mem_profile
+import random
+import time
+
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+print ('Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil()))
+
+"""List"""
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
+
+"""Generator"""
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        yield person
+
+#t1 = time.perf_counter()
+#people = people_list(1000000)
+#t2 = time.perf_counter()
+
+t1 = time.perf_counter()
+people = people_generator(1000000)
+t2 = time.perf_counter()
+
+print ('Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil()))
+print ('Took {} Seconds'.format(t2-t1))
+
+
+
